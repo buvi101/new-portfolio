293d16c97efeddf1f70aa39f623fa5ed2a1ed86a
Bhuwan prasad <nandubuvi@gmail.com>
2025-10-01 22:27:01 +0530

    new resume added

--- React Portfolio/src/Pages/About.jsx
+++ React Portfolio/src/Pages/About.jsx
@@ -31,7 +31,7 @@ function About() {
         <p className='about-para'>An Electronics and Communication Engineer with a deep passion for design and web development. With a strong foundation in HTML, CSS, JavaScript, and React.JS, Enjoy building clean, responsive, and interactive user experiences. Always eager to learn, experiment, and grow as a developer.</p>
       </div>
       <div className="btn-group">
-        <a href="Bhuwan resume.pdf" download target="_blank"><button className="action-btn">Download Resume</button></a>
+        <a href="Bhuwan Prasad J (Final-Resume).pdf" download target="_blank"><button className="action-btn">Download Resume</button></a>
         <a href="https://github.com/buvi101" target="_blank" ><button className="action-btn">Visit Github</button></a>
         <button className='action-btn' onClick={handlemore}>More</button>
       </div>
@@ -39,7 +39,7 @@ function About() {
         ) : (
           <div className='about-sec'>
             <li className='about-para'>
-              Bachelor of Engineering in Electronics and Communication Engineering  |  M.Kumarasamy College of Engeneering  |  6.842 CGPA (Till 5th Semester)
+              Bachelor of Engineering in Electronics and Communication Engineering  |  M.Kumarasamy College of Engeneering  |  6.85 CGPA (Till 6th Semester)
             </li>
             <li className='about-para'>
               Higher Secondary Certificate   |   Meenakshi Sundaranar Sengunthar Higher Secondary School   |   68.67%   |   2022
