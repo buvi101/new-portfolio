c74d769c87bae88157fb1ffe276f528f07fa9b7f
Bhuwan prasad <nandubuvi@gmail.com>
2026-01-02 09:08:59 +0530

    Link App added

--- React Portfolio/src/Pages/WebProject.jsx
+++ React Portfolio/src/Pages/WebProject.jsx
@@ -19,22 +19,22 @@ function WebProject() {
           <div className='text-content'>
             <li>Type: Full-Stack (Personal Project)</li>
             <li>Role: Front-End & Back-End Developer</li>
-            <li>Tech Stack: React.js, Node.js, Express.js, MongoDB</li>
+            <li>Tech Stack: MongoDB, Express.js, React.js, Node.js</li>
             <li>Overview: Echoes of Soul is a full-stack web app with Auth0-based Authentication where users can post, edit, delete their poems and like/comment on others'. Worked as both frontend and backend developer with smooth UX and responsive UI.</li>
           </div>
           <a href="https://echoes-of-soul-front-end.onrender.com/" target="_blank"><button id='project-btn'>Visit</button></a>
         </section>
 
         <section className='SS'>
-          <h1>StaySync</h1>
+          <h1>Link App</h1>
           <img className='ss-img' src="SS pic.png" alt="photo" />
           <div className='text-content'>
-            <li>Type: Full-Stack Team Project (2 Members)</li>
-            <li>Role: Front-End Developer</li>
-            <li>Tech Stack: React.js, Node.js, Express.js, Socket.io, YouTube API</li>
-            <li>Overview: StaySync is a collaborative platform where users can watch YouTube in sync and chat. Worked on designing and developing the front-end interface with a responsive UI.</li>
+            <li>Type: Full-Stack (Personal Project)</li>
+            <li>Role: Front-End & Back-End Developer</li>
+            <li>Tech Stack: React.js, Node.js, Express.js, Socket.io</li>
+            <li>Overview: Link App is a full-stack real-time chat application built with secure JWT-based Authentication, User can login and chat with one-to-one messaging, A responsive and user-friendly interface.</li>
           </div>
-          <button id='project-btn'>Visit</button>
+          <a href="https://linkapp-buvi.netlify.app/" target="_blank"><button id='project-btn'>Visit</button></a>
         </section>
       </div>
     </>
